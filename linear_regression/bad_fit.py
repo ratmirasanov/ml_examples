9282780506b0f081553dd90752b60d67fe14413d
ratmirasanov <ratmir.asanov.7@gmail.com>
2020-05-27 23:51:49 +0300

    Added examples related to a scatter plot and linear regression.

---
+++ linear_regression/bad_fit.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+x = [89, 43, 36, 36, 95, 10, 66, 34, 38, 20, 26, 29, 48, 64, 6, 5, 36, 66, 72, 40]
+y = [21, 46, 3, 35, 67, 95, 53, 72, 58, 10, 26, 34, 90, 33, 38, 20, 56, 2, 47, 15]
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+print(r)
+
+
+def my_func(arg):
+
+    return slope * arg + intercept
+
+
+my_model = list(map(my_func, x))
+plt.scatter(x, y)
+plt.plot(x, my_model)
+plt.show()
